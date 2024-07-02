1e42c5b374c1e68392d308f41a1fbd3d4a9ae225
madhurja <masshuramadhurja99@gmail.com>
2024-07-03 04:34:46 +0600

    Update operation is done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
   const [data, setData] = useState([]);
-
+  const navigate = useNavigate();
   //data fetch from db.json
   useEffect(() => {
     axios
@@ -13,46 +13,65 @@ function Home() {
       .catch((err) => console.assert.log(err));
   }, []);
 
-  return (
+//   const handleDelete=(id)=>{
+//     const confirm  =window.confirm("Would you like to delete?")
+//     if(confirm){
+//         axios.delete("http://localhost:3000/users/"+id)
+//         .then(res=>{
+//             // eslint-disable-next-line no-restricted-globals
+//             location.reload();
+//         }).catch((err) => console.log(err));
+//     }
+};
+
+
+
+return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
       <h1>List of Users</h1>
-      <div className='w-75 rounded  bg-white border shadow p-4'>
-       <div className='d-flex justify-content-end bg-green'>
-        <Link to="/create" className="btn btn-success">Add +</Link> 
+      <div className="w-75 rounded  bg-white border shadow p-4">
+        <div className="d-flex justify-content-end bg-green">
+          <Link to="/create" className="btn btn-success">
+            Add +
+          </Link>
         </div>
-        <table className='table table-stiped'>
+        <table className="table table-stiped">
           <thead>
             <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-          {data.map((d, i) => (
+            {data.map((d, i) => (
               <tr key={i}>
                 <td>{d.id}</td>
                 <td>{d.name}</td>
                 <td>{d.email}</td>
                 <td>{d.phone}</td>
                 <td>
-                {/* <button className="btn btn-sm btn-info me-2">Read</button>
-                  <button className="btn btn-sm btn-primary me-2">Edit</button> */}
-                  <Link to={`/read/${d.id}`} className="btn btn-sm btn-info me-2">Read</Link>
+                  {/* <button className="btn btn-sm btn-info me-2">Read</button>
+                  <button className="btn btn-sm btn-primary me-2">Edit</button> 
+                  <button className="btn btn-sm btn-danger">Delete</button>  */}
+                  <Link to={`/read/${d.id}`}className="btn btn-sm btn-info me-2">
+                    Read
+                  </Link>
 
-<Link to={`/update/${d.id}`} className="btn btn-sm btn-primary me-2">Edit</Link>
-                  <button className="btn btn-sm btn-danger">Delete</button>
+                  <Link to={`/update/${d.id}`} className="btn btn-sm btn-primary me-2">
+                    Edit
+                  </Link>
+                  <button  onClick={e=>handleDelete(d.id)}   className="btn btn-sm btn-danger">Delete</button>
                 </td>
               </tr>
             ))}
-
           </tbody>
         </table>
       </div>
     </div>
   );
-}
 
+}
 export default Home;
