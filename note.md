9ed39fa370a8af3d115ead1e0d3d6483900604c8
madhurja <masshuramadhurja99@gmail.com>
2024-07-03 01:05:03 +0600

    first commit

---
+++ note.md
@@ -0,0 +1,7 @@
+1. create react app
+2. install these packages:
+ npm install react-router-dom axios bootstrap json-server
+3. to start backend server:   json-server --watch db.json
+
+
+* to import bootstrap: import 'bootstrap/dist/css/bootstrap.css';
---
+++ src/Create.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Create() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
+      <div className='w-75 rounded  bg-white border shadow p-4'>
+    <h1>Add a User</h1>
+    <form action="">
+        <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input type="text" name="name" className="form-control" placeholder="Enter Name"/>
+        </div>
+        <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input type="text" name="email" className="form-control" placeholder="Enter Email"/>
+        </div>
+        <div className="mb-2">
+            <label htmlFor="">Phone</label>
+            <input type="text" name="phone" className="form-control" placeholder="Enter Name"/>
+        </div>
+       
+ <button className="btn btn-sm btn-success me-3">Submit</button> 
+  <Link to="/" className="btn btn-sm btn-primary me-3">Back</Link> 
+    </form>
+    
+    
+    </div>
+    </div>
+  )
+}
+
+export default Create
